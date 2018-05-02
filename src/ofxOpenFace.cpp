43ecbfcbb155df9d196b6992444e9989b8011cc7
funkmeisterb <bruno.angeles@mail.mcgill.ca>
2018-05-02 01:31:09 -0400

    Add OpenFace processing logic
    Add the processing logic in the ofxOpenFace class.

--- src/ofxOpenFace.cpp
+++ src/ofxOpenFace.cpp
@@ -1,1 +1,74 @@
 #include "ofxOpenFace.h"
+
+void ofxOpenFace::setup() {
+    imgGrayScale.allocate(640, 480, ofImageType::OF_IMAGE_GRAYSCALE);
+    
+    // Set up OpenFace
+    vector<string> arguments;
+    arguments.push_back("-device");
+    arguments.push_back("0");
+    pDet_parameters = new LandmarkDetector::FaceModelParameters(arguments);
+    
+    // The modules that are being used for tracking
+    string modelLocation = ofFilePath::getAbsolutePath("model/main_clnf_general.txt");
+    pFace_model = new LandmarkDetector::CLNF(modelLocation);
+    
+    if (!pFace_model->eye_model) {
+        ofLogError("No eye model found.");
+    }
+    
+    // A utility for visualizing the results (show just the tracks)
+    pVisualizer = new Utilities::Visualizer(true, false, false, false);
+}
+
+void ofxOpenFace::update(ofImage &img) {
+    imgGrayScale.clone(img);
+    imgGrayScale.setImageType(ofImageType::OF_IMAGE_GRAYSCALE);
+    
+    // Initialize some parameters. See https://github.com/TadasBaltrusaitis/OpenFace/wiki/API-calls
+    float fx = 500.0f;
+    float fy = 500.0f;
+    float cx = 640.0f/2.0f;
+    float cy = 480.0f/2.0f;
+    
+    // Reading the images
+    cv::Mat captured_image = ofxCv::toCv(img.getPixels());
+    cv::Mat grayscale_image = ofxCv::toCv(imgGrayScale.getPixels());
+    
+    // The actual facial landmark detection / tracking
+    bool detection_success = LandmarkDetector::DetectLandmarksInVideo(grayscale_image, *pFace_model, *pDet_parameters);
+    
+    // Gaze tracking, absolute gaze direction
+    cv::Point3f gazeDirection0(0, 0, -1);
+    cv::Point3f gazeDirection1(0, 0, -1);
+    
+    // If tracking succeeded and we have an eye model, estimate gaze
+    if (detection_success && pFace_model->eye_model)
+    {
+        GazeAnalysis::EstimateGaze(*pFace_model, gazeDirection0, fx, fy, cx, cy, true);
+        GazeAnalysis::EstimateGaze(*pFace_model, gazeDirection1, fx, fy, cx, cy, false);
+    }
+    
+    // Work out the pose of the head from the tracked model
+    cv::Vec6d pose_estimate = LandmarkDetector::GetPose(*pFace_model, fx, fy, cx, cy);
+    
+    // Displaying the tracking visualizations
+    pVisualizer->SetImage(captured_image, fx, fy, cx, cy);
+    pVisualizer->SetObservationLandmarks(pFace_model->detected_landmarks, pFace_model->detection_certainty, pFace_model->GetVisibilities());
+    pVisualizer->SetObservationPose(pose_estimate, pFace_model->detection_certainty);
+    pVisualizer->SetObservationGaze(gazeDirection0, gazeDirection1, LandmarkDetector::CalculateAllEyeLandmarks(*pFace_model), LandmarkDetector::Calculate3DEyeLandmarks(*pFace_model, fx, fy, cx, cy), pFace_model->detection_certainty);
+}
+
+
+void ofxOpenFace::draw() {
+    // Draw the visualization
+    cv::Mat matVisualized = pVisualizer->GetVisImage();
+    ofImage imgVisualized;
+    ofxCv::toOf(matVisualized, imgVisualized);
+    imgVisualized.draw(20, 20);
+    imgGrayScale.draw(20 + 640, 20);
+}
+
+void ofxOpenFace::resetFaceModel() {
+    pFace_model->Reset();    
+}
--- src/ofxOpenFace.h
+++ src/ofxOpenFace.h
@@ -6,10 +6,40 @@
 *
 */
 
+#include "ofMain.h"
+#include "ofxCv.h"
+
+// OpenFace
+#include <SequenceCapture.h>
+
+// Libraries for landmark detection (includes CLNF and CLM modules)
+#include "LandmarkCoreIncludes.h"
+#include "GazeEstimation.h"
+
+#include <fstream>
+#include <sstream>
+
+// OpenCV includes
+#include <opencv2/videoio/videoio.hpp>  // Video write
+#include <opencv2/videoio/videoio_c.h>  // Video write
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <Visualizer.h>
+#include <VisualizationUtils.h>
+
 #pragma once
 
 class ofxOpenFace {
-
     public:
-        int                 nTmp;   // WIP
+        void setup();
+        void update(ofImage &img);
+        void draw();
+        void resetFaceModel();
+    
+    private:
+        ofImage                                 imgGrayScale;
+        LandmarkDetector::CLNF*                 pFace_model = nullptr;
+        LandmarkDetector::FaceModelParameters*  pDet_parameters = nullptr;
+        Utilities::Visualizer*                  pVisualizer = nullptr;
 };
