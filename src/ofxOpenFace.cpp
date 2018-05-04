0e6cbbeb3db6963a6c54f8ef797dc81786035e61
funkmeisterb <bruno.angeles@mail.mcgill.ca>
2018-05-03 23:47:02 -0400

    Threaded tracking of the image

--- src/ofxOpenFace.cpp
+++ src/ofxOpenFace.cpp
@@ -1,9 +1,19 @@
 #include "ofxOpenFace.h"
 
+// Constructor
+ofxOpenFace::ofxOpenFace(){
+}
+
+// Destructor
+ofxOpenFace::~ofxOpenFace(){
+    waitForThread(true);
+}
+
 void ofxOpenFace::setup(int nWidth, int nHeight) {
     nImgWidth = nWidth;
     nImgHeight = nHeight;
-    imgGrayScale.allocate(nImgWidth, nImgHeight, ofImageType::OF_IMAGE_GRAYSCALE);
+    imgGrayScale.allocate(nImgWidth, nImgHeight, ofImageType::OF_IMAGE_COLOR);
+    imgVisualized.allocate(nImgWidth, nImgHeight, ofImageType::OF_IMAGE_COLOR);
     
     // Set up OpenFace
     vector<string> arguments;
@@ -23,10 +33,10 @@ void ofxOpenFace::setup(int nWidth, int nHeight) {
     pVisualizer = new Utilities::Visualizer(true, false, false, false);
 }
 
-void ofxOpenFace::update(cv::Mat &mat) {
+void ofxOpenFace::processImage(cv::Mat mat) {
     // Generate grayscale from img
-    ofxCv::toOf(mat, imgGrayScale);
-    imgGrayScale.setImageType(ofImageType::OF_IMAGE_GRAYSCALE);
+    //ofxCv::toOf(mat, imgGrayScale);
+    //imgGrayScale.setImageType(ofImageType::OF_IMAGE_GRAYSCALE);
     
     // Initialize some parameters. See https://github.com/TadasBaltrusaitis/OpenFace/wiki/API-calls
     float fx = 500.0f;
@@ -36,22 +46,17 @@ void ofxOpenFace::update(cv::Mat &mat) {
     
     // Reading the images
     cv::Mat captured_image = mat;
-    cv::Mat grayscale_image = ofxCv::toCv(imgGrayScale.getPixels());
+    //cv::Mat grayscale_image = ofxCv::toCv(imgGrayScale.getPixels());
     
     // The actual facial landmark detection / tracking
-    faceData.detected = LandmarkDetector::DetectLandmarksInVideo(grayscale_image, *pFace_model, *pDet_parameters);
-    
-    // Gaze tracking, absolute gaze direction
-    cv::Point3f gazeDirection0(0, 0, -1);
-    cv::Point3f gazeDirection1(0, 0, -1);
+    //faceData.detected = LandmarkDetector::DetectLandmarksInVideo(grayscale_image, *pFace_model, *pDet_parameters);
+    faceData.detected = LandmarkDetector::DetectLandmarksInVideo(captured_image, *pFace_model, *pDet_parameters);
     
     // If tracking succeeded and we have an eye model, estimate gaze
     if (faceData.detected && pFace_model->eye_model)
     {
-        GazeAnalysis::EstimateGaze(*pFace_model, gazeDirection0, fx, fy, cx, cy, true);
-        GazeAnalysis::EstimateGaze(*pFace_model, gazeDirection1, fx, fy, cx, cy, false);
-        faceData.gazeLeftEye = gazeDirection0;
-        faceData.gazeRightEye = gazeDirection1;
+        GazeAnalysis::EstimateGaze(*pFace_model, faceData.gazeLeftEye, fx, fy, cx, cy, true);
+        GazeAnalysis::EstimateGaze(*pFace_model, faceData.gazeRightEye , fx, fy, cx, cy, false);
     }
     faceData.certainty = pFace_model->detection_certainty;
     
@@ -59,33 +64,55 @@ void ofxOpenFace::update(cv::Mat &mat) {
     faceData.pose = LandmarkDetector::GetPose(*pFace_model, fx, fy, cx, cy);
     
     // Displaying the tracking visualizations
+    mutexVisualizer.lock();
     pVisualizer->SetImage(captured_image, fx, fy, cx, cy);
     pVisualizer->SetObservationLandmarks(pFace_model->detected_landmarks, faceData.certainty, pFace_model->GetVisibilities());
     pVisualizer->SetObservationPose(faceData.pose, faceData.certainty);
     faceData.eyeLandmarks2D = LandmarkDetector::CalculateAllEyeLandmarks(*pFace_model);
     faceData.eyeLandmarks3D = LandmarkDetector::Calculate3DEyeLandmarks(*pFace_model, fx, fy, cx, cy);
     pVisualizer->SetObservationGaze(faceData.gazeLeftEye, faceData.gazeRightEye, faceData.eyeLandmarks2D, faceData.eyeLandmarks3D, faceData.certainty);
+    mutexVisualizer.unlock();
 }
 
-void ofxOpenFace::update(ofImage &img) {
-    update(img.getPixels());
+void ofxOpenFace::setImage(cv::Mat mat) {
+    mutexImage.lock();
+    // Override the current "next image"
+    matToProcess = mat;
+    bHaveNewImage = true;
+    mutexImage.unlock();
 }
 
-void ofxOpenFace::update(ofPixels& pix) {
-    cv::Mat captured_image = ofxCv::toCv(pix);
-    update(captured_image);
+void ofxOpenFace::setImage(ofImage img) {
+    img.setImageType(ofImageType::OF_IMAGE_GRAYSCALE);
+    setImage(img.getPixels());
 }
 
-void ofxOpenFace::draw() {
+void ofxOpenFace::setImage(ofPixels pix) {
+    mutexImage.lock();
+    // Override the current "next image"
+    matToProcess = ofxCv::toCv(pix);
+    bHaveNewImage = true;
+    mutexImage.unlock();
+}
+
+void ofxOpenFace::draw(int x, int y) {
     ofSetColor(ofColor::white);
     // Draw the visualization
-    ofImage imgVisualized;
+    mutexVisualizer.lock();
     ofxCv::toOf(pVisualizer->GetVisImage(), imgVisualized);
-    imgVisualized.draw(20, 20);
+    imgVisualized.draw(x, y);
+    mutexVisualizer.unlock();
+}
+
+void ofxOpenFace::stop() {
+    bExit = true;
+    ofLogNotice("ofxOpenFace", "Stopping thread.");
 }
 
 void ofxOpenFace::exit() {
     // Clear memory
+    stop();
+    waitForThread(true);
     delete pFace_model;
     delete pDet_parameters;
     delete pVisualizer;
@@ -97,4 +124,43 @@ void ofxOpenFace::resetFaceModel() {
 
 const OpenFaceData& ofxOpenFace::getFaceData() {
     return faceData;
+}
+
+void ofxOpenFace::threadedFunction() {
+    thread.setName("ofxOpenFace " + thread.name());
+    ofLogNotice("ofxOpenFace", "Thread started.");
+    
+    while(!bExit) {
+        float fSmoothing = 0.9f;
+        int timeBeforeMs = ofGetElapsedTimeMillis();
+        
+        // Do we have an image to process?
+        if (!bHaveNewImage) {
+            ofSleepMillis(20);
+        } else {
+            //ofLogNotice("ofxOpenFace", "New image to process.");
+            mutexImage.lock();
+            processImage(matToProcess);
+            mutexImage.unlock();
+            bHaveNewImage = false; // ready for a new image
+        }
+        
+        int timeThisRunMs = ofGetElapsedTimeMillis() - timeBeforeMs;
+        fTimePerRunMs = (fTimePerRunMs * fSmoothing) + (timeThisRunMs * (1.0f - fSmoothing)); // time in ms
+        setFPS(1000.0f / fTimePerRunMs);
+    }
+    bHaveNewImage = false;
+}
+
+int ofxOpenFace::getFPS() {
+    mutexFPS.lock();
+    int nToReturn = (int)fFPS;
+    mutexFPS.unlock();
+    return nToReturn;
+}
+
+void ofxOpenFace::setFPS(float value) {
+    mutexFPS.lock();
+    fFPS = value;
+    mutexFPS.unlock();
 }
--- src/ofxOpenFace.h
+++ src/ofxOpenFace.h
@@ -8,6 +8,7 @@
 
 #include "ofMain.h"
 #include "ofxCv.h"
+#include "ofThread.h"
 
 // OpenFace
 #include <SequenceCapture.h>
@@ -32,32 +33,49 @@
 
 class OpenFaceData {
     public:
-        bool                    detected;
+        bool                    detected = false;
         cv::Point3f             gazeLeftEye;
         cv::Point3f             gazeRightEye;
         cv::Vec6d               pose;
         vector<cv::Point2d>     eyeLandmarks2D;
         vector<cv::Point3d>     eyeLandmarks3D;
-        double                  certainty;
+        double                  certainty = 0.0f;
 };
 
-class ofxOpenFace {
+class ofxOpenFace : public ofThread {
     public:
+        ofxOpenFace();
+        ~ofxOpenFace();
         void setup(int nWidth, int nHeight);
-        void update(ofImage& img);
-        void update(cv::Mat& mat);
-        void update(ofPixels& pix);
-        void draw();
+        void processImage(cv::Mat mat);
+        void setImage(ofImage img);
+        void setImage(cv::Mat mat);
+        void setImage(ofPixels pix);
+        void draw(int x, int y);
         void exit();
+        void stop();
         void resetFaceModel();
         const OpenFaceData& getFaceData();
+        int getFPS();
     
     private:
+        virtual void                            threadedFunction();
+        void                                    setFPS(float value);
+    
         int                                     nImgWidth;   // the width of the image used for tracking
         int                                     nImgHeight;  // the height of the image used for tracking
         ofImage                                 imgGrayScale;
+        ofImage                                 imgVisualized;
         LandmarkDetector::CLNF*                 pFace_model = nullptr;
         LandmarkDetector::FaceModelParameters*  pDet_parameters = nullptr;
         Utilities::Visualizer*                  pVisualizer = nullptr;
         OpenFaceData                            faceData;
+        bool                                    bExit = false; // flag to close the thread
+        float                                   fFPS = 0.0f; // thread frame rate
+        ofMutex                                 mutexFPS;
+        ofMutex                                 mutexImage;
+        ofMutex                                 mutexVisualizer;
+        float                                   fTimePerRunMs = 0.0f;
+        bool                                    bHaveNewImage = false; // there is a new image available
+        cv::Mat                                 matToProcess; // the material to process for tracking
 };
